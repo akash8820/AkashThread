4b073daf2e0e379dbff078c56c0bb0bd4482ecff
AKASH <AKASH@192.168.2.11>
2018-03-22 09:31:59 +0530

    Semaphore Example

---
+++ SemaphoreExample/src/com/semaphore/client/ClientTest.java
@@ -0,0 +1,30 @@
+package com.semaphore.client;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.semaphore.connection.Connection;
+
+public class ClientTest {
+	public static void main(String args[]) {
+		ExecutorService executorService = null;
+		try {
+			executorService = Executors.newCachedThreadPool();
+
+			for (int i = 1; i <= 300; i++) {
+				executorService.submit(new Runnable() {
+
+					@Override
+					public void run() {
+						Connection.getConnection().connect();
+					}
+				});
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (executorService != null)
+				executorService.shutdown();
+		}
+	}
+}
